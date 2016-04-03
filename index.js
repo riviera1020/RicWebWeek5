f11370c92ca43541cc22ea69b9238f333369f802
riviera1020 <riviera1020@gmail.com>
2016-04-04 00:06:23 +0800

    GET html done

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+var nunjucks = require('nunjucks');
+var path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'nunjucks');
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+
+router.get('/', function(req, res) {
+	res.render('index.html');
+})
+
+app.use('/', router);
+
+app.use(function(req, res) {
+  res.sendStatus(404);
+});
+
+
+app.listen(3000, function () {
+  console.log('Homework app listening on port 3000!');
+});
